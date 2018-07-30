62c993bafc3458d476d6754d0050dc2756f0dcdb
airtoxin <airtoxin@icloud.com>
2018-07-31 05:45:09 +0900

    Use dynamic import

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # p5ts
+
+## Start
+
+```
+yarn
+yarn start
+```
+
+Open `http://localhost:8080/?sketch=SampleSketch`.
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import "p5";
 import { bindSketchToP5 } from "./Sketch";
-import { SampleSketch } from "./sketches/SampleSketch";
-import { EllipsesSketch } from "./sketches/EllipsesSketch";
-import { StretchingEllipsesSketch } from "./sketches/StretchingEllipsesSketch";
-import { Sketch20180727 } from "./sketches/Sketch20180727";
-import { Sketch2018072801 } from "./sketches/Sketch2018072801";
-import { Sketch2018072802 } from "./sketches/Sketch2018072802";
-import { Sketch2018072803 } from "./sketches/Sketch2018072803";
 
-// tslint:disable-next-line
-new p5(bindSketchToP5(Sketch2018072802));
+const sketch = new URLSearchParams(document.location.search).get("sketch") || "";
+
+import(`./sketches/${sketch}`).then(module => {
+  // tslint:disable-next-line
+  new p5(bindSketchToP5(module[sketch]))
+});
