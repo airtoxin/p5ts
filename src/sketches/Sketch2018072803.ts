79c0368566be3d287a04ff07bf93c5ed2e903ae9
airtoxin <airtoxin@icloud.com>
2018-07-30 14:16:19 +0900

    Add Sketch2018072803

--- src/sketches/Sketch2018072803.ts
+++ src/sketches/Sketch2018072803.ts
@@ -1,24 +1,30 @@
 import { Sketch } from "../Sketch";
 
+const SIZE = 500;
+const NUM_LINES = 100;
+
 export class Sketch2018072803 extends Sketch {
   private canvas: HTMLCanvasElement | undefined;
 
   setup() {
-    const renderer = this.p5.createCanvas(500, 500);
+    const renderer = this.p5.createCanvas(SIZE, SIZE);
     this.canvas = (renderer as any).canvas;
-    this.startCapture();
   }
 
   draw() {
-    this.p5.background(255);
-    for (let i = 0; i < 15; i++) {
-      this.p5.line();
+    this.p5.background(100);
+    this.p5.stroke(255);
+
+    for (let i = 0; i < NUM_LINES; i++) {
+      const x = SIZE / NUM_LINES * (i + 1);
+      const offset = Math.sin(Math.PI * 2 / NUM_LINES * this.p5.frameCount / 3) * SIZE / 2 + SIZE / 2;
+      this.p5.line(x + offset, 100, x, 400);
     }
 
-    this.capture(this.canvas);
-    if (this.p5.frameCount >= 200) {
-      this.stopCapture();
-      this.saveCaptured();
+    for (let i = 0; i < NUM_LINES; i++) {
+      const y = SIZE / NUM_LINES * (i + 1);
+      const offset = Math.sin(Math.PI * 2 / NUM_LINES * this.p5.frameCount / 3) * SIZE / 2 + SIZE / 2;
+      this.p5.line(100, y + offset, 400, y);
     }
   }
 }
