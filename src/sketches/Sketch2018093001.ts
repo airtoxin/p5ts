3e51bc33daabb8f7f03fd9ef31834fbd2a78cb5e
airtoxin <airtoxin@icloud.com>
2018-10-01 22:59:58 +0900

    Add blur

--- src/sketches/Sketch2018093001.ts
+++ src/sketches/Sketch2018093001.ts
@@ -1,12 +1,5 @@
 import { Sketch } from "../Sketch";
-import { range } from "lodash";
-
-interface Rect {
-  xLeftTop: number;
-  yLeftTop: number;
-  xRightBottom: number;
-  yRightBottom: number;
-}
+import { CellArray } from "./CellArray";
 
 export class Sketch2018093001 extends Sketch {
   protected SIZE: number = 200;
@@ -17,67 +10,79 @@ export class Sketch2018093001 extends Sketch {
   private dv: number = 1e-5;
   private f: number = 0.022;
   private k: number = 0.051;
-  private u: number[] = [...Array(this.SIZE * this.SIZE)].map(() => 1);
-  private v: number[] = [...Array(this.SIZE * this.SIZE)].map(() => 0);
-
-  private noiseX = 0;
-  private noiseY = 0;
-  private noiseT = 0;
-  private start = 0;
-  private step = 0.02;
+  private u: CellArray = new CellArray([...Array(this.SIZE * this.SIZE)].map(() => 1), this.SIZE);
+  private v: CellArray = new CellArray([...Array(this.SIZE * this.SIZE)].map(() => 0), this.SIZE);
 
   setup() {
     super.setup();
     this.p5.pixelDensity(1);
     this.initializeDensities();
-    this.p5.noLoop();
+    this.startCapture();
+    // this.p5.noLoop();
   }
 
   draw() {
     super.draw();
 
     this.p5.loadPixels();
-    this.noiseY = this.start;
 
-    for (let y = 0; y < this.p5.height; y++) {
-      for (let x = 0; x < this.p5.width; x++) {
-        const uidx = y * this.SIZE + x;
-        const idx = 4 * uidx;
-        const color = this.u[uidx] * 255;
+    this.updateUv();
+
+    for (let row = 0; row < this.SIZE; row++) {
+      for (let col = 0; col < this.SIZE; col++) {
+        const idx = 4 * (row * this.SIZE + col);
+        const color = this.u.get(row, col) * 255;
         this.p5.pixels[idx] = color;
-        this.p5.pixels[idx+1] = color;
-        this.p5.pixels[idx+2] = color;
-        this.p5.pixels[idx+3] = 255;
+        this.p5.pixels[idx + 1] = color;
+        this.p5.pixels[idx + 2] = color;
+        this.p5.pixels[idx + 3] = 255;
       }
     }
 
     this.p5.updatePixels();
+    this.capture();
+
+    if (this.p5.frameCount >= 200) {
+      this.stopCapture();
+    }
   }
 
   private initializeDensities(): void {
-    const rect: Rect = {
-      xLeftTop: this.SIZE / 2 - 10,
-      yLeftTop: this.SIZE / 2 + 10,
-      xRightBottom: this.SIZE / 2 + 10,
-      yRightBottom: this.SIZE / 2 - 10
-    };
-
-    this.setInRect(
-      rect,
-      () => 0.5 + Math.random() * 0.1,
-      () => 0.25 + Math.random() * 0.1
-    );
+    const rectSize = 20;
+    const borderBottom = this.SIZE / 2 - rectSize / 2;
+    const borderTop = this.SIZE / 2 + rectSize / 2;
+    this.u = this.u.map((cell, row, col) => {
+      if (borderBottom <= row && row <= borderTop && borderBottom <= col && col <= borderTop) {
+        return 0.5 + Math.random() * 0.1;
+      } else {
+        return cell;
+      }
+    });
+    this.v = this.v.map((cell, row, col) => {
+      if (borderBottom <= row && row <= borderTop && borderBottom <= col && col <= borderTop) {
+        return 0.25 + Math.random() * 0.1;
+      } else {
+        return cell;
+      }
+    });
   }
 
-  private setInRect(rect: Rect, fu: (i: number) => number, fv: (i: number) => number): void {
-    for (const i of range(this.SIZE * this.SIZE)) {
-      const row = Math.floor(i / this.SIZE);
-      const col = i % this.SIZE;
-      if (rect.yRightBottom <= row && row <= rect.yLeftTop && rect.xLeftTop <= col && col <= rect.xRightBottom) {
-        // initial value with some noises
-        this.u[i] = fu(i);
-        this.v[i] = fv(i);
-      }
-    }
+  private updateUv(): void {
+    const laplacianU = this.u.roll("up")
+      .add(this.u.roll("down"))
+      .add(this.u.roll("left"))
+      .add(this.u.roll("right"))
+      .sub(this.u.mul(4))
+      .div(this.dx * this.dx);
+    const laplacianV = this.v.roll("up")
+      .add(this.v.roll("down"))
+      .add(this.v.roll("left"))
+      .add(this.v.roll("right"))
+      .sub(this.v.mul(4))
+      .div(this.dx * this.dx);
+    const dudt = laplacianU.mul(this.du);
+    const dvdt = laplacianV.mul(this.dv);
+    this.u = this.u.add(dudt.mul(this.dt));
+    this.v = this.v.add(dvdt.mul(this.dt));
   }
 }
