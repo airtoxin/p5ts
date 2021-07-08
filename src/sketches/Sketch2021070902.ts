bd357832dab2319cd20457c79bc5f22269f6808e
airtoxin <airtoxin@icloud.com>
2021-07-09 07:40:15 +0900

    Add sketch

--- src/sketches/Sketch2021070902.ts
+++ src/sketches/Sketch2021070902.ts
@@ -18,16 +18,19 @@ export class Sketch2021070902 extends Sketch {
     this.p5.noFill();
 
     for (let j = 0; j < 24; j++) {
+      this.p5.translate(this.SIZE / 2, this.SIZE / 2);
+      this.p5.scale(0.4);
       this.p5.rotate(((this.p5.PI * 2) / 24) * j);
 
       this.p5.beginShape();
-      for (let i = 0; i < this.linePoints.length; i++) {
+      for (let i = 0; i < this.linePoints.length + 4; i++) {
         this.p5.curveVertex(
-          this.linePoints[i],
+          this.linePoints[i % this.linePoints.length],
           this.linePoints[(i + 1) % this.linePoints.length]
         );
       }
       this.p5.endShape();
+      this.p5.resetMatrix();
     }
 
     this.p5.pop();
